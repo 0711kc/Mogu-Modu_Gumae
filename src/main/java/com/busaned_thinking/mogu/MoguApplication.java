9198ddf7cb15b6dd12fe96b130625b10988456ec
0711kc <0711kc@naver.com>
2024-07-15 18:07:44 +0900

    initialize files

---
+++ src/main/java/com/busaned_thinking/mogu/MoguApplication.java
@@ -0,0 +1,13 @@
+package com.busaned_thinking.mogu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoguApplication.class, args);
+	}
+
+}
