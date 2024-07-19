2677d9695e2ec811fc395bbceae0372f2f8ec53e
xP-xD <tlsqlfn842@naver.com>
2024-07-19 14:02:04 +0900

    second commit

---
+++ src/main/java/com/busaned_thinking/mogu/user/controller/AskController.java
@@ -0,0 +1,12 @@
+package com.busaned_thinking.mogu.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class AskController {
+}
