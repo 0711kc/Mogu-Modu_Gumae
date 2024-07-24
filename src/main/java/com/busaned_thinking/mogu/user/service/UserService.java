11d0a119b845080bed894a2e7b40ef6d922c9b72
xP-xD <tlsqlfn842@naver.com>
2024-07-24 17:22:43 +0900

    feat: User 프로필 이미지 업데이트 기능 추가

--- src/main/java/com/busaned_thinking/mogu/user/service/UserService.java
+++ src/main/java/com/busaned_thinking/mogu/user/service/UserService.java
@@ -7,8 +7,12 @@ import com.busaned_thinking.mogu.user.controller.dto.request.UpdateUserRequest;
 import com.busaned_thinking.mogu.user.controller.dto.request.UserRequest;
 import com.busaned_thinking.mogu.user.controller.dto.response.UserResponse;
 
+import lombok.NonNull;
+
 public interface UserService {
 	ResponseEntity<UserResponse> createUser(UserRequest userRequest, ActivityArea activityArea);
+
+	ResponseEntity<UserResponse> updateProfileImage(String userId, @NonNull String profileImage);
 
 	ResponseEntity<Void> deleteUser(String userId);
 
