2335b19fe95adb671cf44511db667895a407ac9f
xP-xD <tlsqlfn842@naver.com>
2024-07-24 17:22:43 +0900

    removed cached

--- src/main/java/com/busaned_thinking/mogu/user/controller/UserController.java
+++ src/main/java/com/busaned_thinking/mogu/user/controller/UserController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.busaned_thinking.mogu.image.service.ImageServiceImpl;
 import com.busaned_thinking.mogu.location.entity.ActivityArea;
 import com.busaned_thinking.mogu.location.service.ActivityAreaService;
 import com.busaned_thinking.mogu.user.controller.dto.request.UpdateUserRequest;
@@ -26,6 +29,7 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 	private final UserService userService;
 	private final ActivityAreaService activityAreaService;
+	private final ImageServiceImpl imageServiceImpl;
 
 	@PostMapping("/new")
 	public ResponseEntity<UserResponse> createUser(@RequestBody @Valid final UserRequest userRequest) {
@@ -42,6 +46,13 @@ public class UserController {
 	public ResponseEntity<UserResponse> updateUser(@PathVariable final String userId,
 		@RequestBody @Valid UpdateUserRequest updateUserRequest) {
 		return userService.updateUser(userId, updateUserRequest);
+	}
+
+	@PatchMapping("/{userId}/image")
+	public ResponseEntity<UserResponse> updateProfileImage(@PathVariable final String userId,
+		@RequestPart(name = "image") MultipartFile multipartFile) {
+		String imageLink = imageServiceImpl.upload(multipartFile);
+		return userService.updateProfileImage(userId, imageLink);
 	}
 
 	@DeleteMapping("/{userId}")
