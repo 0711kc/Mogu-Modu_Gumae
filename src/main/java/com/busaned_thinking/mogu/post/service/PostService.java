da571351ccda20971a0aff1cfb8fa36f0ebf2db4
BuildTools <unconfigured@null.spigotmc.org>
2024-07-29 17:13:37 +0900

    refactor: PostDetail 조회와 Post 조회 기능 구분

--- src/main/java/com/busaned_thinking/mogu/post/service/PostService.java
+++ src/main/java/com/busaned_thinking/mogu/post/service/PostService.java
@@ -8,16 +8,19 @@ import com.busaned_thinking.mogu.location.entity.Location;
 import com.busaned_thinking.mogu.post.controller.dto.request.PostRequest;
 import com.busaned_thinking.mogu.post.controller.dto.request.UpdatePostRequest;
 import com.busaned_thinking.mogu.post.controller.dto.response.PostResponse;
+import com.busaned_thinking.mogu.post.controller.dto.response.PostWithDetailResponse;
 
 public interface PostService {
 
 	ResponseEntity<Void> deletePost(String userId, Long postId);
 
-	ResponseEntity<PostResponse> createPost(String memberId, PostRequest postRequest, Location location,
+	ResponseEntity<PostWithDetailResponse> createPost(String memberId, PostRequest postRequest, Location location,
 		List<String> postImageLinks);
 
 	ResponseEntity<PostResponse> findPost(Long id);
 
-	ResponseEntity<PostResponse> updatePost(String userId, Long postId, UpdatePostRequest updatePostRequest,
+	ResponseEntity<PostWithDetailResponse> findPostWithDetail(Long id);
+
+	ResponseEntity<PostWithDetailResponse> updatePost(String userId, Long postId, UpdatePostRequest updatePostRequest,
 		List<String> postImageLinks, Location location);
 }
