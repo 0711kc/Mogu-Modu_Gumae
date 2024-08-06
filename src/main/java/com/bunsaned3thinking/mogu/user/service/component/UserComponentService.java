1b38c9edf984aed1abb3572875b46d30b131cd7f
BuildTools <unconfigured@null.spigotmc.org>
2024-08-06 21:01:52 +0900

    feat: User 비밀번호 변경 기능 추가, UpdateUtil 추가

--- src/main/java/com/bunsaned3thinking/mogu/user/service/component/UserComponentService.java
+++ src/main/java/com/bunsaned3thinking/mogu/user/service/component/UserComponentService.java
@@ -3,6 +3,7 @@ package com.bunsaned3thinking.mogu.user.service.component;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.bunsaned3thinking.mogu.user.controller.dto.request.UpdateUserPasswordRequest;
 import com.bunsaned3thinking.mogu.user.controller.dto.request.UpdateUserRequest;
 import com.bunsaned3thinking.mogu.user.controller.dto.request.UserRequest;
 import com.bunsaned3thinking.mogu.user.controller.dto.response.UserResponse;
@@ -16,4 +17,6 @@ public interface UserComponentService {
 
 	ResponseEntity<UserResponse> updateUser(String userId, UpdateUserRequest updateUserRequest,
 		MultipartFile multipartFile);
+
+	ResponseEntity<UserResponse> updateUserPassword(String userId, UpdateUserPasswordRequest updateUserPasswordRequest);
 }
