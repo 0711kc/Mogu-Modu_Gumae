3213440c3eb95297b56e379f3cd7702948ded4ad
0711kc <0711kc@naver.com>
2024-08-19 16:46:40 +0900

    refactor: 유효성 검사 시 메시지 추가

--- src/main/java/com/bunsaned3thinking/mogu/user/controller/dto/request/UpdateUserRequest.java
+++ src/main/java/com/bunsaned3thinking/mogu/user/controller/dto/request/UpdateUserRequest.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 public class UpdateUserRequest {
-	@Size(max = 12)
+	@Size(max = 12, message = "닉네임의 최대 크기는 12글자입니다.")
 	private String nickname;
 
 	@DecimalMin(value = "-180.0", message = "-180.0 ~ 180.0 사이의 숫자를 입력해주세요")
@@ -28,7 +28,7 @@ public class UpdateUserRequest {
 	@DecimalMax(value = "90.0", message = "-90.0 ~ 90.0 사이의 숫자를 입력해주세요")
 	private Double latitude;
 
-	@Size(min = 500, max = 3000)
+	@Size(min = 500, max = 3000, message = "활동 범위는 500~3000 사이의 숫자로 입력해주세요")
 	private Short distanceMeters;
 
 	public static UpdateUserRequest from(User user) {
