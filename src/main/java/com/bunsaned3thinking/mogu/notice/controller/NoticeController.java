b4eede4dd79c5b500457e11e9b8c5a4e2d2fe1cb
0711kc <0711kc@naver.com>
2025-02-14 18:12:46 +0900

    feat: 공지사항 스웨거 추가

--- src/main/java/com/bunsaned3thinking/mogu/notice/controller/NoticeController.java
+++ src/main/java/com/bunsaned3thinking/mogu/notice/controller/NoticeController.java
@@ -18,38 +18,54 @@ import com.bunsaned3thinking.mogu.notice.controller.dto.response.NoticeHeadRespo
 import com.bunsaned3thinking.mogu.notice.controller.dto.response.NoticeResponse;
 import com.bunsaned3thinking.mogu.notice.service.NoticeService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@Tag(name = "공지사항 API", description = "공지사항 관련 API 입니다.")
 @RequestMapping("/notice")
 @RequiredArgsConstructor
 public class NoticeController {
 	private final NoticeService noticeService;
 
 	@PostMapping
+	@Operation(summary = "공지사항 생성", description = "공지사항 정보를 생성합니다.")
 	public ResponseEntity<NoticeResponse> createNotice(@RequestBody @Valid final NoticeRequest noticeRequest) {
 		return noticeService.createNotice(noticeRequest);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<NoticeResponse> findNotice(@PathVariable final Long id) {
+	@Operation(summary = "공지사항 조회", description = "공지사항 정보를 조회합니다.")
+	public ResponseEntity<NoticeResponse> findNotice(
+		@Parameter(name = "Notice ID", description = "공지사항의 id", in = ParameterIn.PATH)
+		@PathVariable final Long id) {
 		return noticeService.findNotice(id);
 	}
 
 	@GetMapping("/all")
+	@Operation(summary = "모든 공지사항 조회", description = "모든 공지사항 정보를 조회합니다.")
 	public ResponseEntity<List<NoticeHeadResponse>> findAllNotice() {
 		return noticeService.findAllNotice();
 	}
 
 	@PatchMapping("/{id}")
-	public ResponseEntity<NoticeResponse> updateNotice(@PathVariable final Long id,
+	@Operation(summary = "공지사항 수정", description = "공지사항 정보를 수정합니다.")
+	public ResponseEntity<NoticeResponse> updateNotice(
+		@Parameter(name = "Notice ID", description = "공지사항의 id", in = ParameterIn.PATH)
+		@PathVariable final Long id,
 		@RequestBody @Valid final UpdateNoticeRequest updateNoticeRequest) {
 		return noticeService.updateNotice(id, updateNoticeRequest);
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteNotice(@PathVariable final Long id) {
+	@Operation(summary = "공지사항 삭제", description = "공지사항 정보를 삭제합니다.")
+	public ResponseEntity<Void> deleteNotice(
+		@Parameter(name = "Notice ID", description = "공지사항의 id", in = ParameterIn.PATH)
+		@PathVariable final Long id) {
 		return noticeService.deleteNotice(id);
 	}
 
