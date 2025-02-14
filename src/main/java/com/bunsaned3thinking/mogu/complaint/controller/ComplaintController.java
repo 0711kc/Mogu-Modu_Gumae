aeed28d8ce51207eb1a48cda3be62eba078af979
0711kc <0711kc@naver.com>
2025-02-14 18:17:17 +0900

    feat: 문의 스웨거 추가

--- src/main/java/com/bunsaned3thinking/mogu/complaint/controller/ComplaintController.java
+++ src/main/java/com/bunsaned3thinking/mogu/complaint/controller/ComplaintController.java
@@ -22,42 +22,59 @@ import com.bunsaned3thinking.mogu.complaint.controller.dto.response.ComplaintHea
 import com.bunsaned3thinking.mogu.complaint.controller.dto.response.ComplaintResponse;
 import com.bunsaned3thinking.mogu.complaint.service.component.ComplaintComponentService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
+@Tag(name = "문의 API", description = "문의 관련 API 입니다.")
 @RequestMapping("/complaint")
 @RequiredArgsConstructor
 public class ComplaintController {
 	private final ComplaintComponentService complaintComponentService;
 
 	@PostMapping
+	@Operation(summary = "문의 생성", description = "문의 정보를 생성합니다.")
 	public ResponseEntity<ComplaintResponse> createComplaint(
 		Principal principal,
+		@Parameter(description = "생성할 문의 정보")
 		@RequestPart(name = "request") @Valid final ComplaintRequest complaintRequest,
+		@Parameter(description = "문의 이미지 리스트")
 		@RequestPart(value = "multipartFileList", required = false) Optional<List<MultipartFile>> multipartFileList) {
 		return complaintComponentService.createComplaint(complaintRequest, multipartFileList.orElseGet(ArrayList::new),
 			principal.getName());
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<ComplaintResponse> findComplaint(@PathVariable final Long id, Principal principal) {
+	@Operation(summary = "문의 조회", description = "문의 정보를 조회합니다.")
+	public ResponseEntity<ComplaintResponse> findComplaint(
+		@Parameter(name = "Complaint ID", description = "문의의 id", in = ParameterIn.PATH)
+		@PathVariable final Long id, Principal principal) {
 		return complaintComponentService.findComplaint(id, principal.getName());
 	}
 
 	@GetMapping("/all")
+	@Operation(summary = "모든 문의 조회", description = "모든 문의 정보를 조회합니다.")
 	public ResponseEntity<List<ComplaintHeadResponse>> findAllComplaint() {
 		return complaintComponentService.findAllComplaint();
 	}
 
 	@GetMapping("/all/my")
+	@Operation(summary = "나의 모든 문의 조회", description = "본인이 작성한 모든 문의 정보를 조회합니다.")
 	public ResponseEntity<List<ComplaintHeadResponse>> findAllComplaintByUser(Principal principal) {
 		return complaintComponentService.findAllComplaintByUser(principal.getName());
 	}
 
 	@PatchMapping("/{id}")
-	public ResponseEntity<ComplaintResponse> updateComplaint(@PathVariable final Long id,
+	@Operation(summary = "문의 수정", description = "문의 정보를 수정합니다.")
+	public ResponseEntity<ComplaintResponse> updateComplaint(
+		@Parameter(name = "Complaint ID", description = "문의의 id", in = ParameterIn.PATH)
+		@PathVariable final Long id,
 		Principal principal,
+		@Parameter(description = "수정할 문의 정보")
 		@RequestBody @Valid UpdateComplaintRequest updateComplaintRequest) {
 		return complaintComponentService.updateComplaint(id, updateComplaintRequest, principal.getName());
 	}
